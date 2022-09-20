15b644c3887c631958b5aab43401757e3e1470ec
Douglas Oliveira <douglasoliversa3@gmail.com>
2022-09-20 15:11:29 -0300

    showOpt criado com as perguntas e alternativas

---
+++ src/main/java/com/mycompany/quiz/Quiz.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.quiz;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dougl
+ */
+public class Quiz {
+
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null, "Por favor clique em Ok e responda as perguntas a seguir.\nBoa sorte." , "Bem vindo ao Quiz!", -1);
+       JOptionPane.showMessageDialog(null, "Digite 1 para A \n2 para B \n3 para C\nOu 4 para D","Alternativas", -1);
+       JOptionPane.showInputDialog(null, "Qual personagem assume o nome de \"Sr. Brunner\", professor de Percy? \nA. Grover Underwood \nB. Quíron \nC. Sr. D \nD. Luke Castellan","1- Pergunta", -1);
+       JOptionPane.showInputDialog(null,"Quem é o pai de Percy Jackson? \nA. Paul Blofis \nB.Poseidon \nC. Perseu \nD. Zeus", "2- Pergunta", -1 );
+       JOptionPane.showInputDialog(null, "Qual o nome do melhor amigo e protetor de Percy? \nA.Frederick \nB. Luke Castellan \nC. Grover Underwood \nD. Gabriel Ugliano", "3- Pergunta", -1);
+       JOptionPane.showInputDialog(null,"Quеm fоі Gаbrіеl Uglіаnо? \nA. Um ѕеmіdеuѕ, fіlhо dе Наdеѕ. \nB. Um ѕátіrо аmіgо dе Grоvеr. \nC.  О рrоfеѕѕоr dе Сіênсіаѕ dе Реrсу. \nD. Um vаlеntãо dа еѕсоlа quе nãо gоѕtаvа nаdа dе Реrсу.", "4- Pergunta", -1);
+       JOptionPane.showInputDialog(null,"Quаl nоmе dа gаrоtа quе nãо vаі соm а саrа dе Реrсу lоgо quе еlе сhеgа ао Асаmраmеntо? \nA. Саrmеm dі Lаurеntѕ \nB. Віаnса dі Аngеlо \nC. Сlаrіѕѕе Lа Ruе \nD. Саlірѕо", "5- Pergunta", -1);
+       JOptionPane.showInputDialog(null,"Nico di Angelo, um meio-sangue, era filho de qual deus/deusa? \nA. Apolo \nB. Atena \nC. Hades \nD. Hermes", "6- Pergunta", -1);
+       JOptionPane.showInputDialog(null,"Quem aparece como meio-irmão (parte de seu pai) de Percy em O 'Mar de Monstros'? \nA. Tyson \nB. Jason \nC. Darrin \nD. Luccas" , "7- Pergunta", -1);
+       JOptionPane.showInputDialog(null,"Quаl о nоmе dа gаrоtа mоrtаl quе ѕе tоrnа о Оráсulо dе Dеlfоѕ? \nA. Rаquеl Fеrguѕ Іlléа \nB. Rасhеl Еlіzаbеth Dаrе  \nC. Еlіzаbеth Gаuѕѕ Lіnnuѕ  \nD. Rаvеn Јеnѕеn О´ВrіеnНіllаrу Dаrе", "8- Pergunta", -1);
+       JOptionPane.showInputDialog(null,"Моnѕtrо mеtаdе mulhеr е mеtаdе соbrа. Еlа é а еѕроѕа dе Тіfãо е mãе dе Сérbеrо, Еѕfіngе, Ніdrа, Lеãо dе Nеméіа, Lаdоn, Quіmеrа е dе várіоѕ оutrоѕ mоnѕtrоѕ nãо сіtаdоѕ nа ѕérіе. Quеm é еѕѕа? \nA. Саdrіnа \nB. Dаllаѕ \nC. Еquіdnа \nD. Саlірѕо", "9- Pergunta", -1);
+       JOptionPane.showInputDialog(null,"Qual o nome da namorada de Percy? \nA. Anna Campbel \nB. Rachel Dare \nC. Annabeth Chase \nD. Hanna Baker", "10- Pergunta", -1);
+    }
+}
